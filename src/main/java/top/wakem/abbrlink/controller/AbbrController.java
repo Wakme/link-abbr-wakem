485f4031264566f88cf66bda838d160359541230
Wakem <44000839@qq.com>
2022-09-11 01:02:30 +0800

    fix: index接口误删除

--- src/main/java/top/wakem/abbrlink/controller/AbbrController.java
+++ src/main/java/top/wakem/abbrlink/controller/AbbrController.java
@@ -1,12 +1,8 @@
 package top.wakem.abbrlink.controller;
 
-import freemarker.template.utility.DateUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.thymeleaf.util.DateUtils;
 import top.wakem.abbrlink.common.enums.BizExceptionEnum;
 import top.wakem.abbrlink.common.exception.BizException;
 import top.wakem.abbrlink.common.request.LinkRequest;
@@ -14,12 +10,8 @@ import top.wakem.abbrlink.common.response.BaseResponse;
 import top.wakem.abbrlink.service.AbbrService;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.*;
 
 @Controller
 public class AbbrController {
@@ -32,6 +24,13 @@ public class AbbrController {
     public BaseResponse<String> addLink(@RequestBody LinkRequest linkRequest) {
         String abbr = abbrService.addLink(linkRequest.getLink(), linkRequest.getExpireDays());
         return BaseResponse.success(abbr);
+    }
+
+    @GetMapping("/index")
+    public ModelAndView test() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        return mv;
     }
 
     @GetMapping("/{abbr}")

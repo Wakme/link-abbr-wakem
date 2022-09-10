de9e2e020cfbbfb925abe56a8a4685dace312bc1
Wakem <44000839@qq.com>
2022-09-11 00:58:38 +0800

    fix: update

--- src/main/java/top/wakem/abbrlink/interceptor/MyExceptionHandler.java
+++ src/main/java/top/wakem/abbrlink/interceptor/MyExceptionHandler.java
@@ -2,14 +2,16 @@ package top.wakem.abbrlink.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import top.wakem.abbrlink.common.enums.BizExceptionEnum;
 import top.wakem.abbrlink.common.exception.BizException;
 import top.wakem.abbrlink.common.response.BaseResponse;
 
 @Controller
 @Slf4j
-public class ExceptionHandler {
+public class MyExceptionHandler {
 
+    @ExceptionHandler(BizException.class)
     public BaseResponse<String> bizExceptionHandler(BizException e) {
         log.error("业务异常", e);
         return BaseResponse.fail(BizExceptionEnum.SYSTEM_ERROR);
